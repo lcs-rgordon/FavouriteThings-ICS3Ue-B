2944f44f10d39584a14ed656d3bb24f2087657f3
Russell Gordon <rgordon@lcs.on.ca>
2021-02-07 14:45:22 -0500

    Finished up with an explanation of why Piper is one of my favourite things.

--- FavouriteThings/BlueJays.swift
+++ FavouriteThings/BlueJays.swift
@@ -18,9 +18,9 @@ struct BlueJays: View {
             Text("""
                 Growing up in Kitchener-Waterloo, I spent a lot of time with family that lived in areas nearby. My Aunt Phyllis and Uncle Bert in particular introduced me to baseball – or more specificially – Blue Jays baseball!
 
-                Lloyd Moseby, Dave Stieb, George Bell, Ernie Whitt... these were the Blue Jays of my youth. The sound of Tom Cheek and Jerry Howarth calling a game on the radio was the background to many  summer days.
+                Lloyd Moseby, Dave Stieb, George Bell, Ernie Whitt... these were the Blue Jays of my youth. The sound of Tom Cheek and Jerry Howarth calling a game on the radio was the background to many summer days.
 
-                More recently I lived in down Toronto during the Blue Jays big runs of 2015 and 2016. I was in the Rogers Centre for Jose Bautista's "bat flip" home run, which is a moment I will never forget.
+                More recently I lived in downtown Toronto during the Blue Jays big runs of 2015 and 2016. I was in the Rogers Centre for Jose Bautista's "bat flip" home run, which is a moment I will never forget.
 
                 I now have many fond memories of bringing my own daughter, Claire, to Blue Jays games as she grew up. Once the pandemic is over, I am looking forward to getting back out to ballgames with my friends and family.
                 """)
--- FavouriteThings/Piper.swift
+++ FavouriteThings/Piper.swift
@@ -15,7 +15,18 @@ struct Piper: View {
                 .resizable()
                 .scaledToFit()
             
-            Text("Piper is fuzzy and adorable and I love her very much.")
+            Text("""
+                Just under three years ago, my family welcomed a new member to our pack: Piper!
+
+                Piper as a puppy packed a lot of pep! She was full of energy. While we were fortunate that she never showed much interest in chewing the furniture – something young Labradors are known for – she had no such compunction about my ankles. There was a period when she was about 10 weeks old where I would only go in the kitchen – the part of the house she was limited to at first – when wearing knee high rainboots. Protection. 😅
+
+                My family thinks Piper at any age was adorable, but as a puppy she definitely had her moments. A favourite memory is watching Piper chew a stick with her "best friend"... the yellow Labrador who lives next door and is about five years older than Piper.
+
+                These days Piper has slowed down a bit (she loves to sleep on our couch) but when she goes out for a walk (often at Northcote) she will tear around with complete abandon.
+
+                We love Piper very much. 🥰
+                """)
+                .padding()
             
         }
         .navigationTitle("Piper")
