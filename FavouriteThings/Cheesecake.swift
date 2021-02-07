1a6b9140d7e24822ac00d4ac60870c6c10bcba58
Russell Gordon <rgordon@lcs.on.ca>
2021-02-07 14:34:54 -0500

    Why cheesecake is awesome.

--- FavouriteThings/Cheesecake.swift
+++ FavouriteThings/Cheesecake.swift
@@ -15,8 +15,19 @@ struct Cheesecake: View {
                 .resizable()
                 .scaledToFit()
             
-            Text("How many ways can I tell you how much I love cheesecake?")
-            
+            Text("""
+                I am well known in my family for my love of cheesecake. Whenever we would eat out at a restaurant as I was growing up, if we stayed for dessert, the first thing I would look for on the menu is cheesecake.
+
+                First things first: "supermarket cheesecake" is not real cheesecake. Sorry, Sara Lee.
+
+                What makes a good cheesecake? First, the filling. It shouldn't taste like a block of cheese... but it should also not be light, like a cream pie. There is a happy medium.
+
+                Second, the crust. A cheesecake's crust should be thick, with a good amount of crumble and no small amount of brown sugar. Not too hard, but defintely not soggy.
+
+                Finally, I like to compare apples to apples. The classic "New York style" cheesecake is best. Variants like chocolate cheesecake are OK, but my own preference is to stick with the standard.
+                """)
+                .padding()
+
         }
         .navigationTitle("Cheesecake")
     }
