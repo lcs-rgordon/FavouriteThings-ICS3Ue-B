88d9a1298945a527c8c001fa9759edcd34d168a5
Russell Gordon <rgordon@lcs.on.ca>
2021-02-07 14:21:48 -0500

    Added more text for the Blue Jays page.

--- FavouriteThings/BlueJays.swift
+++ FavouriteThings/BlueJays.swift
@@ -15,7 +15,16 @@ struct BlueJays: View {
                 .resizable()
                 .scaledToFit()
             
-            Text("Growing up, my family would always have the Blue Jays game on in the background.")
+            Text("""
+                Growing up in Kitchener-Waterloo, I spent a lot of time with family that lived in areas nearby. My Aunt Phyllis and Uncle Bert in particular introduced me to baseball – or more specificially – Blue Jays baseball!
+
+                Lloyd Moseby, Dave Stieb, George Bell, Ernie Whitt... these were the Blue Jays of my youth. The sound of Tom Cheek and Jerry Howarth calling a game on the radio was the background to many  summer days.
+
+                More recently I lived in down Toronto during the Blue Jays big runs of 2015 and 2016. I was in the Rogers Centre for Jose Bautista's "bat flip" home run, which is a moment I will never forget.
+
+                I now have many fond memories of bringing my own daughter, Claire, to Blue Jays games as she grew up. Once the pandemic is over, I am looking forward to getting back out to ballgames with my friends and family.
+                """)
+                .padding()
             
         }
         .navigationTitle("Blue Jays")
